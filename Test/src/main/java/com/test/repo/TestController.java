d0e7692765478f566210c1b9673ea519108df510
lee su ho <leeshsla1@naver.com>
2023-01-16 15:43:17 +0900

    테스트 파일 실행성공하고 연습하는 중

---
+++ Test/src/main/java/com/test/repo/TestController.java
@@ -0,0 +1,27 @@
+package com.test.repo;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping("/")
+public class TestController {
+	
+	
+	@RequestMapping(value = "test")
+	public String test() {
+		return "test";
+	}
+	
+}
---
+++ Test/src/main/webapp/WEB-INF/views/test.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>test</title>
+</head>
+<body>
+<h1>
+	Hello world!  
+</h1>
+
+<h3>깃허브 테스트용 실행 파일입니다.</h3>
+<h3>push 연습중...</h3>
+</body>
+</html>
